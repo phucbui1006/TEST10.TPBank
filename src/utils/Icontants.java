099d61d3abb3f6b0b003ca84a79a3921d173ae3f
Phuc Bui <phuchoangb228@gmail.com>
2026-05-12 07:51:34 +0700

    sửa lại quy ước password

--- src/utils/Icontants.java
+++ src/utils/Icontants.java
@@ -14,5 +14,5 @@ public class Icontants {
     
     public static int CAPCHAR_LENGTH = 5; // độ dài của mã capcha cố định là 5
     public static String ACCOUNT = "^[0-9]{10}$"; // Tài khoản ngân hàng từ 0-9 và chỉ có 10 kí tự
-    public static String PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,31}$"; // có đủ a-z, có số và gồm 8-31 kí tự
+    public static String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,31}$"; // có đủ chữ hoa, chữ thường, có số và gồm 8-31 kí tự
 }
