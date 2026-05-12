73ae9069fc968f678b551f3bd184a7a084ad9a18
Phuc Bui <phuchoangb228@gmail.com>
2026-05-12 12:57:45 +0700

    done mã regex

--- src/utils/Icontants.java
+++ src/utils/Icontants.java
@@ -14,5 +14,5 @@ public class Icontants {
     
     public static int CAPCHAR_LENGTH = 5; // độ dài của mã capcha cố định là 5
     public static String ACCOUNT = "^[0-9]{10}$"; // Tài khoản ngân hàng từ 0-9 và chỉ có 10 kí tự
-    public static String PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,31}$"; // có đủ a-z, có số và gồm 8-31 kí tự
+    public static String PASSWORD = "^(?=.*[a-z])(?=.*\\d)(?=.*[\\W_])[a-z\\d\\W_]{8,31}$"; // có chữ thường, có số, kí tự đặc biệt và gồm 8-31 kí tự
 }
